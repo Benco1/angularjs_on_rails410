a7dfcb909bb9a03f5d5d1ef036c33be03cac19e3
Benco <bcos34@gmail.com>
2015-02-24 18:41:58 -0500

    Add Raffler code up to highlighting last winner

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,4 +1,27 @@
 angular.module('myApp')
 	.controller('HomeCtrl', function ($scope) {
-			$scope.things = ['Angular', 'Rails 4.1', 'UI Router', 'Together!!'];
+			$scope.entries = [
+
+				{name: 'Angular'},
+				{name: 'Rails 4.1'},
+				{name: 'UI Router'},
+				{name: 'Together!!'}
+				];
+
+			$scope.addEntry = function () {
+				$scope.entries.push($scope.newEntry)
+				$scope.newEntry = {}
+			}
+
+			$scope.drawWinner = function () {
+				var randNum = Math.floor(Math.random() * $scope.entries.length);
+				var entry = $scope.entries[randNum];
+				entry.winner = true;
+				$scope.lastWinner = entry;
+			}
+
+			$scope.clearEntries = function () {
+				$scope.entries = [];
+			}
 	});
+
